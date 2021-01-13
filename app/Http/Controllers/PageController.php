38fc27d1e7968b3907a17a30710de1fb1266e7cf
= <=>
2021-01-14 00:14:13 +0700

    Updates function Checkout

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -5,6 +5,11 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Slide;
 use App\Product;
+use App\Customer;
+use App\Bills;
+use App\BillDetail;
+use App\Cart;
+use Session;
 
 class PageController extends Controller
 {
@@ -39,5 +44,43 @@ class PageController extends Controller
      public function getAbout()
     {
         return view('frontend.pages.about');
+    }
+
+    public function getCheckout()
+    {
+        $cart = new Cart();
+        return view('frontend.pages.checkout',compact('cart'));
+    }
+
+    public function postCheckout(Request $request)
+    {
+       $cart = new Cart();
+
+       $customer = new Customer();
+       $customer->fullname      = $request->txtFullname;
+       $customer->email         = $request->txtEmail;
+       $customer->address       = $request->txtAddress;
+       $customer->phone_number  = $request->txtPhone;
+       $customer->note          = $request->txtNote;
+       $customer->save();
+
+       $bill = new Bills();
+       $bill->id_customer = $customer->id;
+       $bill->date_order = date('Y-m-d');
+       $bill->total = $cart->totalPrice;
+       $bill->payment = $request->txtPayment;
+       $bill->note = $request->txtNote;
+       $bill->save();
+
+       foreach($cart->items as $key => $item){
+            $bill_detail = new BillDetail();
+            $bill_detail->id_bill = $bill->id;
+            $bill_detail->id_product = $key;
+            $bill_detail->quantity = $item['quantity'];
+            $bill_detail->unitprice = $item['price'];
+            $bill_detail->save();
+       }
+       Session::forget('cart');
+       return redirect()->back()->with('success','Đặt Hàng Thành Công !!!');
     }
 }
