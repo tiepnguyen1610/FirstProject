06b594117ca23c66281c6678aa81d10c7ec2e340
= <=>
2021-01-14 00:17:28 +0700

    create table bills

---
+++ database/migrations/2021_01_13_205939_create_bills_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBillsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bills', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_customer');
+            $table->date('date_order');
+            $table->integer('total');
+            $table->string('payment');
+            $table->text('note');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bills');
+    }
+}
